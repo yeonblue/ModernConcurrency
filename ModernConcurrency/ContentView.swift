251411fbe80a6ff32947f10aa4839b4bc78b74de
최서연 <yeondublue@naver.com>
2022-10-11 19:46:32 +0900

    first commit

---
+++ ModernConcurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModernConcurrency
+//
+//  Created by yeonBlue on 2022/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModernConcurrency/ModernConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernConcurrencyApp.swift
+//  ModernConcurrency
+//
+//  Created by yeonBlue on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct ModernConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
